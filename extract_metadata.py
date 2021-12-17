7d9de180138f783700148bf860f22351da8a75fb
Michael Haslam <hreikin@gmail.com>
2021-12-17 14:28:14 +0000

    Processes the metadata into a dictionary and adds the number of pages info to it. Splits the keyword metadata and creates a list from the output which is added to the processed metadatas dictionary. Also makes the output file name match the input file name with a different extension.

--- extract_metadata.py
+++ extract_metadata.py
@@ -11,25 +11,34 @@ def get_metadata(input_file):
         pdf_file_reader = PdfFileReader(metadata_file)
         metadata = pdf_file_reader.getDocumentInfo()
         number_of_pages = pdf_file_reader.getNumPages()
+        pages_dict = {"/NumberOfPages": number_of_pages}
 
-    # Prints out the metadata and number of pages to the console for 
-    # verification purposes. The metadata is sorted alphabetically and indented 
-    # with 4 spaces. The number of pages is not exported to the JSON 
-    # automatically so we add it to the metadata.
-    json_object = json.dumps(metadata, sort_keys = True, indent = 4)
-    print("Metadata that was found:\n", json_object)
-    print(f"Number of pages: {number_of_pages}")
-    print("Adding Number Of Pages value to metadata.")
-    pages_dict = {"/NumberOfPages": number_of_pages}
-    metadata.update(pages_dict)
+    # Processes the "/Keywords" metadata to strip the whitespace from within the 
+    # string and then creates a list from the remaining items by using the comma 
+    # as the delimiter.
+    keyword_string = str()
+    for k, v in metadata.items():
+        if k == "/Keywords":
+            keyword_string += v
+        keyword_list = keyword_string.split(", ")
+
+    # Creates the "processed_metadata" dictionary from items extracted from the 
+    # PDF's metadata, then adds the "number_of_pages" dictionary and 
+    # "keyword_list" list items as entries.
+    processed_metadata = dict()
+    processed_metadata["Author"] = metadata["/Author"]
+    processed_metadata["Title"] = metadata["/Title"]
+    processed_metadata["Subject"] = metadata["/Subject"]
+    processed_metadata["NumberOfPages"] = pages_dict["/NumberOfPages"]
+    processed_metadata["Keywords"] = keyword_list
 
     # Creates output file in write mode ("w"). Sorts the "key : value" pairs 
     # alphabetically and indents 4 spaces when exporting.
     with open(output_file, "w") as metadata_json_file:
-        print("Exporting metadata to JSON file:", output_file)
-        json.dump(metadata, metadata_json_file, sort_keys = True, indent = 4)
+        print("Exporting processed metadata to JSON file:", output_file)
+        json.dump(processed_metadata, metadata_json_file, sort_keys = True, indent = 4)
 
 if __name__ == "__main__":
     input_file = "meta_data_test.pdf"
-    output_file = "metadata.json"
+    output_file = input_file.replace(".pdf", ".json")
     get_metadata(input_file)
